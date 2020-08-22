e686e8b3cc70119f2e178ceb5927f10a66301ea3
gzstyp <444141300@qq.com>
2020-08-22 08:34:55 +0800

    实例一个对象

--- src/config/service.js
+++ src/config/service.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 //创建axios实例,本实例不包含表单里有文件上传功能
-const service = axios.create({
+const instance = axios.create({
     timeout : 10000
 });
 
 //请求拦截器,好使!!!但需要后端要支持才行
-service.interceptors.request.use(function(config){
+instance.interceptors.request.use(function(config){
     var access = sessionStorage.getItem('access_token');
     var refresh = sessionStorage.getItem('refresh_token');
     if(access != null && access.length > 0){
@@ -32,13 +32,13 @@ service.interceptors.request.use(function(config){
 });
 
 //响应拦截器,在实际应用中可以,好使!!!
-service.interceptors.response.use((data) =>{
+instance.interceptors.response.use((data) =>{
     var _data = data.data;
     if(_data.code === 200){
         var renewal = data.renewal;//注意该更新token标识
         if(renewal){
             //更新令牌
-            service.get('http://api.fwtai.com/dataView/getFloorArea', {params: {}}).then(data =>{
+            instance.get('http://api.fwtai.com/dataView/getFloorArea', {params: {}}).then(data =>{
                 console.info('更新令牌成功');
             }).catch(error=>{
                 console.info('更新令牌失败');
@@ -50,4 +50,4 @@ service.interceptors.response.use((data) =>{
     console.info(error);
 });
 
-export default service;//导出，方便其他调用!!!
+export default instance;//导出，方便其他调用!!!
